a625a9bb46b9c5a500fc1d1455567c6259b09ccb
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-03-23 15:38:50 +0100

    fixed libivy tap

--- libivy.rb
+++ libivy.rb
@@ -3,19 +3,19 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Libivy < Formula
   desc ""
-  homepage ""
-  url "file:///Users/mpoirier/Documents/Git/libivy-3.15.zip"
-  sha256 "77ed4b691139dc00e33cd95ea838552333c469cd0400f6cb07f00811591cfee5"
+  homepage "https://github.com/lii-enac/libivy"
+  url "https://github.com/lii-enac/libivy/archive/3.15.zip"
+  sha256 "4daa1e5c4c9da271579ff6d5d164d130a8f41390823775232c2b388fef246f0d"
 
   depends_on "glib"
 
   def install
     ENV.deparallelize
-    system "cd", "trunk/src", ";", "make", "-j",  "-f",  "Makefile.osx", "static-libs", "shared-libs"
+    system "cd", "src", ";", "make", "-j",  "-f",  "Makefile.osx", "static-libs", "shared-libs"
 
-    include.install "trunk/src/ivy.h", "trunk/src/ivybind.h", "trunk/src/ivybuffer.h", "trunk/src/ivychannel.h"
-    include.install "trunk/src/ivydebug.h", "trunk/src/ivyloop.h", "trunk/src/ivysocket.h", "trunk/src/list.h"
-    include.install "trunk/src/timer.h", "trunk/src/version.h", "trunk/src/ivyglibloop.h"
-    lib.install "trunk/src/libivy.a", "trunk/src/libivy.dylib", "trunk/src/libglibivy.a", "trunk/src/libglibivy.dylib"
+    include.install "src/ivy.h", "src/ivybind.h", "src/ivybuffer.h", "src/ivychannel.h"
+    include.install "src/ivydebug.h", "src/ivyloop.h", "src/ivysocket.h", "src/list.h"
+    include.install "src/timer.h", "src/version.h", "src/ivyglibloop.h"
+    lib.install "src/libivy.a", "src/libivy.dylib", "src/libglibivy.a", "src/libglibivy.dylib"
   end
 end
