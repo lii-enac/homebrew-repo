32ce7040cc241e27c55146290250d38c22febe79
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-04-02 16:54:50 +0200

    smala.rb - release 1.9.0

--- smala.rb
+++ smala.rb
@@ -4,20 +4,16 @@
 class Smala < Formula
   desc "Compiler for the Smala language"
   homepage ""
-  url "https://github.com/lii-enac/smala/archive/1.8.0.zip"
-  sha256 "db2b8b2089c50ece8f11dffd0306fa4c2a92abb7d3e40ac5508dfe34a1a84b74"
-
-  depends_on "djnn-cpp" => "1.9.0"
+  url "https://github.com/lii-enac/smala/archive/1.9.0.zip"
+  sha256 "406ad54a87451a71d926d489b9ca0077a54fc5455b10543797b38e21ba1e135e"
+  
+  depends_on "djnn-cpp" => "1.10.0"
   depends_on "bison" => "3.5.3"
 
   def install
     ENV.deparallelize
 
-    system "make", "-j"
-    #system "make", "-j", "cookbook_apps"
-
-    bin.install "build/smalac"
-    #share.install Dir["build/*/*/*_app", "build/*/*/*/*_app"]
+    system "make", "-j", "install", "prefix=#{prefix}"
   end
 
   test do
