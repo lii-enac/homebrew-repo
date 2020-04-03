f3d62a597264f893fd5e8ce60dde873df2d78455
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-04-03 17:10:40 +0200

    djnn-cpp - fixed

--- djnn-cpp.rb
+++ djnn-cpp.rb
@@ -5,13 +5,15 @@ class DjnnCpp < Formula
   desc "C++ library for interactive software programming "
   homepage "https://github.com/lii-enac/djnn-cpp"
   url "https://github.com/lii-enac/djnn-cpp/archive/1.10.0.zip"
+  sha256 "59b53cfdf60364a4e1abd706cf0c28a73c4eab657782935585dd441295712ea6"
 
   depends_on "qt" => "5.14"
   depends_on "flex" => "2.6.4"
   depends_on "libivy" => "3.15"
 
   def install
-    ENV.deparallelize  
+    ENV.deparallelize
+    ENV['CXX'] = "c++" 
     system "make", "-j", "install", "prefix=#{prefix}"
   end
 
