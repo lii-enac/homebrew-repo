e35e05399243f496830df450a40e2572a4418d9e
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-04-24 10:55:08 +0200

    fixed sha256

--- djnn-cpp.rb
+++ djnn-cpp.rb
@@ -5,7 +5,7 @@ class DjnnCpp < Formula
   desc "C++ library for interactive software programming "
   homepage "https://github.com/lii-enac/djnn-cpp"
   url "https://github.com/lii-enac/djnn-cpp/archive/1.10.0.zip"
-  sha256 "59b53cfdf60364a4e1abd706cf0c28a73c4eab657782935585dd441295712ea6"
+  sha256 "08a44315f5119fe9e166b024d92a25fae5cd47cb5cf27930415975741e19ddb6"
 
   depends_on "qt" => "5.14"
   depends_on "flex" => "2.6.4"
