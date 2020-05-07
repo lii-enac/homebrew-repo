8c28d5105ef42c213d104904313545b822516cc7
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-05-07 17:02:18 +0200

    release smala 1.10.1

--- smala.rb
+++ smala.rb
@@ -4,10 +4,10 @@
 class Smala < Formula
   desc "Compiler for the Smala language"
   homepage ""
-  url "https://github.com/lii-enac/smala/archive/1.9.0.zip"
-  sha256 "406ad54a87451a71d926d489b9ca0077a54fc5455b10543797b38e21ba1e135e"
+  url "https://github.com/lii-enac/smala/archive/1.10.1.zip"
+  sha256 "c2cc274d36851547e77349fd5a5436ec4d86e2a64e5d5244185421b211c44b5d"
   
-  depends_on "djnn-cpp" => "1.10.0"
+  depends_on "djnn-cpp" => "1.11.0"
   depends_on "bison" => "3.5.3"
 
   def install
