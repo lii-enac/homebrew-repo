a612b9c63d1f01e667bfba742d13f93712fbd02d
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-06-25 15:28:06 +0200

    libivy.rb - new version 3.15.2

--- libivy.rb
+++ libivy.rb
@@ -4,8 +4,8 @@
 class Libivy < Formula
   desc ""
   homepage "https://github.com/lii-enac/libivy"
-  url "https://github.com/lii-enac/libivy/archive/3.15.zip"
-  sha256 "4daa1e5c4c9da271579ff6d5d164d130a8f41390823775232c2b388fef246f0d"
+  url "https://github.com/lii-enac/libivy/archive/3.15.2.zip"
+  sha256 "ba90608d8e335d002595430a5695575138a9f54c26a52703bc7cd3a3208820a9"
 
   depends_on "glib"
   depends_on 'pkg-config'
