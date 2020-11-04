e57bb5300ea986eba8ec447b3f20980207a7a918
Mathieu Poirier <mathieu.poirier@enac.fr>
2020-11-04 16:14:06 +0100

    smala.rb - prepare for 1.14.0 release

--- smala.rb
+++ smala.rb
@@ -4,16 +4,16 @@
 class Smala < Formula
   desc "Compiler for the Smala language"
   homepage ""
-  url "https://github.com/lii-enac/smala/archive/1.13.0.zip"
-  sha256 "12643849ea5ecd31de63f1075c1c86721c80afcc6df40d0d5e362c1a35131f3e"
+  url "https://github.com/lii-enac/smala/archive/1.14.0.zip"
+  sha256 ""
   
-  depends_on "djnn-cpp" => "1.13.0"
+  depends_on "djnn-cpp" => "1.14.0"
   depends_on "bison" => "3.5.3"
 
   def install
     ENV.deparallelize
 
-    system "make", "-j", "install_brew", "prefix=#{prefix}"
+    system "make", "-j", "install_brew", "PREFIX=#{prefix}"
   end
 
   test do
