5d0d420813df0d14a938989bfbdec2bac4726b63
Mathieu Poirier <mathieu.poirier@enac.fr>
2021-03-18 13:05:16 +0100

    libivy - fixed deponds_on

--- libivy.rb
+++ libivy.rb
@@ -7,7 +7,7 @@ class Libivy < Formula
   url "https://github.com/lii-enac/libivy/archive/3.15.4.zip"
   sha256 "de2563a1c86a219333f6eecc3cacb7c65dab848938a4ebdb80b714bf9fe6c14f"
 
-  depends_on "glib"
+  depends_on "pcre"
   depends_on 'pkg-config'
 
   def install
