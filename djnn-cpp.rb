c591a4568966343fde10cbdcd48ca40d161d1edb
Mathieu Poirier <mathieu.poirier@enac.fr>
2021-03-18 11:40:51 +0100

    djnn-cpp.rb - fixed depends_on

--- djnn-cpp.rb
+++ djnn-cpp.rb
@@ -7,7 +7,8 @@ class DjnnCpp < Formula
   url "https://github.com/lii-enac/djnn-cpp/archive/1.15.0.zip"
   sha256 "bc3a37ad42e6b6201f01c1672b1ac43c04b2435dc4201e12e19d1c63e3f4a91c"
 
-  depends_on "qt" => "5.15"
+  depends_on "qt@5" => "5.15.2"
+  depends_on "bison" => "3.5.7"
   depends_on "flex" => "2.6.4"
   depends_on "boost" => "1.74.0"
   depends_on "libivy" => "3.15.3"
