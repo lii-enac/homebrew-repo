d7ab4f3b134691d3cb993dafa3d3f357443c775e
Mathieu Poirier <mathieu.poirier@enac.fr>
2023-10-11 17:54:56 +0200

    smala - release 1.20.0 + brew audit

--- smala.rb
+++ smala.rb
@@ -1,20 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Smala < Formula
   desc "Compiler for the Smala language"
-  homepage ""
-  url "https://github.com/lii-enac/smala/archive/1.19.0.zip"
-  sha256 "ae49c919cd60a8f081a051b052f9dc4158b12b115114910933c6faa43a727d68"
-  
-  depends_on "djnn-cpp" => "1.19.0"
-  depends_on "bison" => "3.5.3"
+  homepage "https://github.com/lii-enac/smala"
+  url "https://github.com/lii-enac/smala/archive/1.20.0.tar.gz"
+  sha256 "7d6d61cee9998454248b26ea155728f8f6736d6cdb912b9cb53f1c334dd67c50"
+
+  depends_on "bison"
+  depends_on "djnn-cpp"
   depends_on "flex"
 
   def install
     ENV.deparallelize
-
-    system "make", "-j", "install_brew", "PREFIX=#{prefix}"
+    system "make", "-j8", "install_brew", "PREFIX=#{prefix}"
   end
 
   test do
