2a46bf8f904f0a966bcaef5313761d8628728ceb
Mathieu Poirier <mathieu.poirier@enac.fr>
2024-03-12 14:20:16 +0100

    libivy - release 3.15.8

--- libivy.rb
+++ libivy.rb
@@ -1,8 +1,8 @@
 class Libivy < Formula
   desc "Library for bus Ivy (ENAC)"
   homepage "https://github.com/lii-enac/libivy"
-  url "https://github.com/lii-enac/libivy/archive/3.15.7.tar.gz"
-  sha256 "c7410dcb62a6fc71f71b5ea536439292fc002182376738eb8bf90212d368b496"
+  url "https://github.com/lii-enac/libivy/archive/3.15.8.tar.gz"
+  sha256 "78754acfb1ea67e6748d7e3e9bb2e554fc1abcefe78edfde270bdae56599f288"
 
   depends_on "pcre"
   depends_on "pkg-config"
