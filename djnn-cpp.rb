60cba42e542a0ae033a02d5ffa7d6821634725dd
Mathieu Poirier <mathieu.poirier@enac.fr>
2024-07-17 16:51:15 +0200

    djnn-cpp release 1.21.0

--- djnn-cpp.rb
+++ djnn-cpp.rb
@@ -1,8 +1,8 @@
 class DjnnCpp < Formula
   desc "C++ library for interactive software programming"
   homepage "https://github.com/lii-enac/djnn-cpp"
-  url "https://github.com/lii-enac/djnn-cpp/archive/1.20.0.tar.gz"
-  sha256 "175d8a298ffb5a40a49d7c594f9ffdb3a6e956a8406cab0c1fb4478504cfd6fa"
+  url "https://github.com/lii-enac/djnn-cpp/archive/1.21.0.tar.gz"
+  sha256 "fe4361f3a906fb240317b3acdd3461fc01f6ed2fb106e73a6e8f2b971ade2a33"
 
   depends_on "bison"
   depends_on "curl"
