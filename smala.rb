90788bff0711295480483f97d46d26ddff5cf0b1
Mathieu Poirier <mathieu.poirier@enac.fr>
2025-07-31 16:05:16 +0200

    smala release 1.22.0

--- smala.rb
+++ smala.rb
@@ -1,8 +1,8 @@
 class Smala < Formula
   desc "Compiler for the Smala language"
   homepage "https://github.com/lii-enac/smala"
-  url "https://github.com/lii-enac/smala/archive/1.21.0.tar.gz"
-  sha256 "c8becd2639a2f8730f070b47cec6195195bd14324ec15ca8a0c316d3c4d3ea57"
+  url "https://github.com/lii-enac/smala/archive/1.22.0.tar.gz"
+  sha256 "1ab3cdf761b726cd3caf5335f65736b66a9133b13f159d7e647567f80697941d"
 
   depends_on "bison"
   depends_on "djnn-cpp"
@@ -10,6 +10,7 @@ class Smala < Formula
 
   def install
     ENV.deparallelize
+    ENV["CXX"] = ENV.cxx.to_s
     system "make", "-j8", "install_brew", "PREFIX=#{prefix}"
   end
 
