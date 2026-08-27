12c9163ad027fac9564526bc4d02f9569dd18204
Mathieu Poirier <mathieu.poirier@enac.fr>
2026-08-27 17:10:37 +0200

    smala release 1.23.0

--- smala.rb
+++ smala.rb
@@ -1,8 +1,8 @@
 class Smala < Formula
   desc "Compiler for the Smala language"
   homepage "https://github.com/lii-enac/smala"
-  url "https://github.com/lii-enac/smala/archive/1.22.0.tar.gz"
-  sha256 "1ab3cdf761b726cd3caf5335f65736b66a9133b13f159d7e647567f80697941d"
+  url "https://github.com/lii-enac/smala/archive/1.23.0.tar.gz"
+  sha256 "8c9bff793bd18404e14a6fc868c86590d114fb55e3bdd89dbecb38effb546bd1"
 
   depends_on "bison"
   depends_on "djnn-cpp"
